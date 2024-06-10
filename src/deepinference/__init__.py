6e28b467c362a625113788214ba4b1135124191f
Andrea Ravenni <andrea.ravenni@unipd.it>
2024-06-10 16:34:08 +0200

    Remove old code

--- src/deepinference/__init__.py
+++ src/deepinference/__init__.py
@@ -8,9 +8,15 @@ else:
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "DeepInference"
+    dist_name = __name__
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
+
+from ._deep_inference import *
+from . import data_preprocessing
+from . import visualization
+from . import losses
+from . import activations
