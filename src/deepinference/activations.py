76dfb5a446790243779ca24088abe1b1fd339fb1
Andrea Ravenni <andrea.ravenni@unipd.it>
2024-06-11 17:11:55 +0200

    Add docs 1/n

--- src/deepinference/activations.py
+++ src/deepinference/activations.py
@@ -2,7 +2,22 @@ import tensorflow as _tf
 from tensorflow.keras import layers as _layers
 
 def elu_plus_one(input):
-    """ Adds one to the Exponential Linear Unit
+    """ Exponential Linear Unit plus one.
+
+    Adds a constant A to the exponential linear unit (ELU) such that
+    the output is guaranteed to be positive. A=1+1.e-7. Notice that
+    if A=1 numerical instabilities can make the output negative.
+    See the (elu documentation)[https://www.tensorflow.org/api_docs/python/tf/keras/activations/elu].
+
+    Parameters
+    ----------
+    input : tensor
+        Input tensor
+
+    Returns
+    -------
+    tensor
+
     """
     return _tf.add(_tf.nn.elu(input),
                    _tf.constant(1.0000001, dtype=_tf.float32) # Notice I added 1.e-7 for stability
--- src/deepinference/data_preprocessing.py
+++ src/deepinference/data_preprocessing.py
@@ -7,6 +7,32 @@ def train_val_test_split(
         test_fraction,
         random_state=1,
 ):
+    """Split data in train, validation and test set.
+
+    Split data in train, validation and test set according to the fraction selected
+    applying twice the scikit learn routine train_test_split.
+    See the (train_test_split documentation)[https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html].
+
+    Parameters
+    ----------
+    properties : indexable with same lenght/shape[0] as labels
+        Tabular input features. Lenght/shape[0] is the number of data samples.
+    labels : indexable with samel lenght/shape[0] as properties
+        Tabular target variables.
+    val_fraction : float
+        Fraction of samples to be included in the validation set.
+    test_fraction : float
+        Fraction of samples to be included in the test set.
+    random_state : int
+        Control the shuffling applied to data before applying the split. Same value
+        guarantees reproducible output.
+
+    Returns
+    -------
+    list of length=6
+        List of train, validation, and test properties, train, validation, and test labels.
+
+    """
     test_size = round(len(properties) * test_fraction)
     val_size = round(len(properties) * val_fraction)
 
@@ -17,7 +43,7 @@ def train_val_test_split(
     train_prop, val_prop, train_lab, val_lab = _train_test_split(
           train_prop, train_lab,
           test_size=val_size, random_state=random_state)
-    
+
     return train_prop, val_prop, test_prop, train_lab, val_lab, test_lab
 
 
@@ -27,11 +53,12 @@ def get_data_dict(
         val_fraction = 0.2,
         test_fraction = 0.2,
         random_state = 1,
-):    
+):
     """returns a dictionary with the input data divided in input and target variables.
     Each is further divided in train validation and test set
-    
-    Arguments:
+
+    Parameters
+    ----------
     data : ndarray
         assumed to be 2d. The second index labels the variables. 
         The input variables follow the target variables.
@@ -43,17 +70,23 @@ def get_data_dict(
         fraction of data to be used in the test set. Default = 0.2.
     random_state : int
         seed of the rng. Default = 1
+
+    Returns
+    -------
+    dictionary
+        Dictionary with 3 different items train, val, and test, further subdivided in 
+        ftr (features) and lbl (labels)
+
     """
-    
     data_dict = {
         "train" : {},
         "val" : {},
         "test" : {},
     }
-    
+
     lbl = data[:,0:first_feature_index].copy()
-    ftr = data[:,first_feature_index:].copy()    
-    
+    ftr = data[:,first_feature_index:].copy()
+
     data_dict["train"]["ftr"],\
     data_dict["val"]["ftr"],\
     data_dict["test"]["ftr"],\
@@ -66,5 +99,5 @@ def get_data_dict(
         test_fraction,
         random_state,
     )
-    
+
     return data_dict
